cae15da2ef6fc2fb0c85df5356e07f395e6ff86c
AimanFred <irzanfarish@gmail.com>
2022-08-19 21:49:23 +0800

    add Image logo and metadata

--- pages/create.js
+++ pages/create.js
@@ -1,8 +1,16 @@
+import Head from 'next/head'
+
 const Create = () => {
   return ( 
-    <div className="create">
-      <h1>New Issue</h1>
-    </div>
+    <>
+      <Head>
+        <title>Issue Tracker | New Issue</title>
+        <meta name="keyword" content="issues" />
+        </Head>
+      <div className="create">
+        <h1>New Issue</h1>
+      </div>
+    </>
    );
 }
  
