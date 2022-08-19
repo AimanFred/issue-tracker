fe0c52bb59ccf394e54a8db8a4b77461b8c11f3b
AimanFred <irzanfarish@gmail.com>
2022-08-19 20:23:18 +0800

    add create and issues pages

---
+++ pages/create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return ( 
+    <div className="create">
+      <h1>New Issue</h1>
+    </div>
+   );
+}
+ 
+export default Create;
---
+++ pages/issues/index.js
@@ -0,0 +1,10 @@
+const IssueList = () => {
+  return ( 
+    <div className="issues-list">
+      <h1>All Issues</h1>
+    </div>
+   );
+}
+ 
+export default IssueList
+;
